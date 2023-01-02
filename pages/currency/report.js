241031582ca78680696648abc434ba21ba923599
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-02 18:11:29 -0300

    fetching data from report page

--- pages/currency/report.js
+++ pages/currency/report.js
@@ -1,13 +1,40 @@
 import { useRouter } from "next/router"
+import { useState } from "react"
+import style from '../../styles/report.module.css'
 
-export default ()=>{
-    const router = useRouter()
-    const {source, currencies} = router.query
+export async function getServerSideProps({query}) {
+
+    const {source, currencies} = query
 
     if(!source || !currencies)
-        return <>
+        return { props: { data : null } }
+
+    const res = await fetch(`https://api.apilayer.com/currency_data/live?source=${source}&currencies=${currencies}`, 
+    {
+        headers:{
+            "apikey": process.env.API_KEY
+        }
+    })
+   
+  
+    const data = await res.json()
+  
+    return { props: {data}   }
+}
+
+export default ({data})=>{
+    if(!data){
+        return <div className="container">
         <h1>Houve algum erro</h1>
-        <a href="currency">Voltar</a>
-        </>
-    return <h1>Report</h1>
+        <a className="primary-button" href="/currency">Voltar</a>
+        </div>
+    }
+
+    const [cant, setCant] = useState(1)
+
+    return <div className="container">
+    <div className={style.first_text}>
+        Quanto custa <input onInput={e=>setCant(e.target.value)} value={cant} type="number" min="0" className={"custom-input " + style.input}/> {data.source}
+    </div>
+    </div>
 }
