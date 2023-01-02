60e727060447ec9a4f8f562641712253693d95f9
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-02 14:19:07 -0300

    add css code for custom select

--- components/convert.js
+++ components/convert.js
@@ -9,6 +9,15 @@ const options = [
 export default ({price}) =>{
     return <>
         <h1>Conversor</h1>
-
+        <div>
+        <input type="number" class="custom-input" min="0"/>
+        <h1>BTC</h1>
+        <input type="number" class="custom-input" min="0"/>
+        <select class="custom-select">
+            <option>1</option>
+            <option>1</option>
+            <option>1</option>
+        </select>
+        </div>
     </>
 }
