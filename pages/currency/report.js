3109f4e2ede6af563734fa60340434a8aa7f378a
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-02 17:36:39 -0300

    add report page

--- pages/currency/report.js
+++ pages/currency/report.js
@@ -1,3 +1,13 @@
+import { useRouter } from "next/router"
+
 export default ()=>{
+    const router = useRouter()
+    const {source, currencies} = router.query
+
+    if(!source || !currencies)
+        return <>
+        <h1>Houve algum erro</h1>
+        <a href="currency">Voltar</a>
+        </>
     return <h1>Report</h1>
 }
