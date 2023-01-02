a3bdb05edea885f643eff13a470814df84577d27
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-02 18:24:43 -0300

    blank index page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
-import Bitcoin from "../components/bitcoin"
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res_price = await fetch('https://api.blockchain.info/ticker')
-  const res_var = await fetch('https://api.blockchain.info/charts/market-price?timespan=6days&format=json')
-
-  const price = await res_price.json()
-  const variation = await res_var.json()
-
-  return { props: {price, variation}   }
-}
 
 export default ({price, variation})=>{
 
   return <div className="container">
-    <Bitcoin price={price} variation={variation}/>
+    <h1>Hola</h1>
   </div>
 }
