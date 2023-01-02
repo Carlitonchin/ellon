6e5862585ebbe1ab01c93b316912283edd43ce8f
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-02 20:32:34 -0300

    starting with the header design

--- components/header.js
+++ components/header.js
@@ -1,3 +1,15 @@
+import Link from "next/link"
+
 export default ()=>{
-    return <div>header</div>
+    return <header>
+        <nav>
+            <h1>Logo</h1>
+
+            <ul>
+                <li><Link href="/">Inicio</Link></li>
+                <li><Link href="/bitcoin">Bitcoins</Link></li>
+                <li><Link href="/currency">Moedas Fiat</Link></li>
+            </ul>
+        </nav>
+    </header>
 }
