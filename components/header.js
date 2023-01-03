b8c25cffb1c3cfec884c3856fe9f4aa25674f0bb
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 17:08:48 -0300

    n-fixixng bug

--- components/header.js
+++ components/header.js
@@ -33,10 +33,16 @@ const Header = ({routes, currentPage})=>{
 
     useEffect(()=>{
         window.addEventListener('click', (e)=>{
+            console.log(e.target.id)
+            if(e.target.id == 'menu_mobile')
+                return
+
+            else if(e.target.id == "menu_icon" || e.target.id == "container_menu_icon"){
+                return
+            }
             
-            if(e.target.id != 'menu_mobile' && e.target.id != "menu_icon")
+            else
                 close_menu_if_possible()
-            
             
         })
     })
@@ -53,8 +59,10 @@ const Header = ({routes, currentPage})=>{
                 })}
                 
             </ul>
-
-            <svg onClick={toggle_menu} id="menu_icon" className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            
+            <div id="container_menu_icon" onClick={toggle_menu}>
+            <svg id="menu_icon" className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            </div>
         </nav>
 
     </header>
