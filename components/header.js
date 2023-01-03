c4ba9aa548df75b3062ab5b303fbd5d4bdb1b439
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 16:12:58 -0300

    menu animation finished

--- components/header.js
+++ components/header.js
@@ -1,9 +1,15 @@
 import Link from "next/link"
 
 const Header = ({routes, currentPage})=>{
-    function click_menu(){
+
+    function toggle_menu(){
         let menu_mobile = document.getElementById("menu_mobile")
-        menu_mobile.style.marginTop = 0
+
+        if(menu_mobile.classList.contains('show-menu-mobile'))
+            menu_mobile.classList.remove('show-menu-mobile')
+        
+        else
+            menu_mobile.classList.add('show-menu-mobile')
     }
     return <header>
         <nav>
@@ -18,7 +24,7 @@ const Header = ({routes, currentPage})=>{
                 
             </ul>
 
-            <svg onClick={click_menu} className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            <svg onClick={toggle_menu} className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
         </nav>
 
     </header>
