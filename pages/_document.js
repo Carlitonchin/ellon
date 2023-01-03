09fa13865501d743c7099191c538885c1895c1bd
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 17:41:12 -0300

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <title>Ellon Bank</title>
         <meta name="description" content="Ferramenta online para traders" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <body>
         <Main />
