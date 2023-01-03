874dbd13f9dfa53f2fd16b36413e3182e22814b0
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 16:29:06 -0300

    menu mobile animation finished

--- components/header.js
+++ components/header.js
@@ -1,16 +1,55 @@
 import Link from "next/link"
+import { useEffect } from "react"
 
 const Header = ({routes, currentPage})=>{
 
-    function toggle_menu(){
+    function close_menu_if_possible(){
         let menu_mobile = document.getElementById("menu_mobile")
 
-        if(menu_mobile.classList.contains('show-menu-mobile'))
+        if(menu_mobile.classList.contains('show-menu-mobile')){
             menu_mobile.classList.remove('show-menu-mobile')
-        
-        else
-            menu_mobile.classList.add('show-menu-mobile')
+
+            menu_mobile.childNodes.forEach(e=>{
+                e.classList.remove('show-options')
+            })
+            
+            return true
+        }
+
+        return false
     }
+
+    function show_menu_if_possible(){
+        let menu_mobile = document.getElementById("menu_mobile")
+
+        if(!menu_mobile.classList.contains('show-menu-mobile')){
+            menu_mobile.classList.add('show-menu-mobile')
+
+            setTimeout(()=>{
+                menu_mobile.childNodes.forEach(e=>{
+                    e.classList.add('show-options')
+                })
+            }, 300)
+
+            return true
+    }
+    return false
+}
+
+    function toggle_menu(){
+        if(!close_menu_if_possible())
+            show_menu_if_possible()
+    }
+            
+
+    useEffect(()=>{
+        window.addEventListener('click', (e)=>{
+            if(e.target.id != 'menu_mobile' && e.target.id != "menu_icon")
+                close_menu_if_possible()
+            
+        })
+    })
+
     return <header>
         <nav>
             <Link href="/">
@@ -24,7 +63,7 @@ const Header = ({routes, currentPage})=>{
                 
             </ul>
 
-            <svg onClick={toggle_menu} className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            <svg onClick={toggle_menu} id="menu_icon" className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
         </nav>
 
     </header>
