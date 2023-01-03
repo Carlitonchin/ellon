2983a660d2505337714f89eb7e9b6c1eadac9073
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 13:50:50 -0300

    working on header menu for mobil

--- components/header.js
+++ components/header.js
@@ -6,14 +6,24 @@ const Header = ({routes, currentPage})=>{
             <Link href="/">
             <h3 className="logo">Ellon<span>BANK</span></h3>
             </Link>
-            <ul>
+            <ul className="menu-pc">
                 {Object.keys(routes).map(url=>{
                     return <li key={url}><Link style={{color:currentPage == url ? 'gold' : ""}} href={url}>{routes[url]}</Link></li>
                 
                 })}
                 
             </ul>
+
+            <svg className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
         </nav>
+
+        <ul className="menu-mobile">
+                {Object.keys(routes).map(url=>{
+                    return <li key={url}><Link style={{color:currentPage == url ? 'gold' : ""}} href={url}>{routes[url]}</Link></li>
+                
+                })}
+                
+            </ul>
     </header>
 }
 
