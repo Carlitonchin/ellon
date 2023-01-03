f92fc52c0ec55ba0880b21f0b2ce5220ba7d0b64
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 17:33:58 -0300

    add metadata

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt">
-      <Head />
+      <Head>
+        <title>Ellon Bank</title>
+        <meta name="description" content="Ferramenta online para traders" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
