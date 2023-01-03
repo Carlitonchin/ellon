0189caa0ce692ae9d2cd8b2446b04d5aaf394bea
Carlitonchin <carlozalejandro98@gmail.com>
2023-01-03 17:31:58 -0300

    settimeout for not conflict at events

--- components/header.js
+++ components/header.js
@@ -26,14 +26,16 @@ const Header = ({routes, currentPage})=>{
 }
 
     function toggle_menu(){
+        
         if(!close_menu_if_possible())
             show_menu_if_possible()
+       
+        
     }
             
 
     useEffect(()=>{
         window.addEventListener('click', (e)=>{
-            console.log(e.target.id)
             if(e.target.id == 'menu_mobile')
                 return
 
@@ -60,7 +62,9 @@ const Header = ({routes, currentPage})=>{
                 
             </ul>
             
-            <div id="container_menu_icon" onClick={toggle_menu}>
+            <div id="container_menu_icon" onClick={()=> setTimeout(() => {
+                toggle_menu()
+            }, 100)}>
             <svg id="menu_icon" className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
             </div>
         </nav>
